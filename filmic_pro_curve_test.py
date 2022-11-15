059341a983acc8349a9ca1e3148fc4c8e7398256
Freeman, Thatcher Solomon <thatcher@gatech.edu>
2022-11-14 23:18:17 -0800

    added filmic log v3 paper, but it's kinda bullshit

---
+++ filmic_pro_curve_test.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def mix(x, y, t):
+    return (x + (y - x) * t)
+
+def lin2log(inVec):
+    l = (np.log2(inVec) + 8.0) * 0.125
+    x = mix(np.maximum(0.0, l), inVec, 0.1267)
+    y = 0.28985507246
+    outVec = np.maximum(0.0, mix(x, np.power(inVec, y), inVec))
+    return outVec
+
+def log2lin(inVec):
+    u = np.exp2((inVec / 0.125) - 8.0)
+    x = 1.1612159730893894
+    y = 0.6090138106343165
+    outVec = np.power(u, mix(x, y, inVec))
+    return outVec
+
+x = np.linspace(0.0, 1.0, 1024)
+y = lin2log(log2lin(x))
+
+plt.plot(x, y, label="lin2log(log2lin(x))")
+plt.plot([0.0, 1.0], [0.0, 1.0], label="y=x")
+plt.legend()
+plt.show()
